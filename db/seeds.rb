daa104bfe267df2aed64c9772cee179faa3524aa
Gabriel Alarcon <galarconesp@gmail.com>
2018-09-06 20:21:29 -0300

    commit 06: seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(title: 'Tomar un terremoto', photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Terremotopiojera.jpg/320px-Terremotopiojera.jpg' )
+Task.create(title: 'Ir a las ramadas', photo: 'https://diarioelnortino.cl/wp-content/uploads/2016/09/Ramadas-por-fiestas-patrias-1024x680.jpg')
+Task.create(title: 'Encumbrar volantines', photo: 'http://www.bibliotecaviva.cl/wp-content/uploads/2014/05/volantines21.jpg')
+Task.create(title: 'Tomar chicha', photo: 'http://static.t13.cl/images/sizes/1440x692/mgr_630_chicha.jpg')
+Task.create(title: 'Comer choripanes', photo: 'https://static1.squarespace.com/static/535a4c40e4b01ad3a32bae36/538c9167e4b06d612ab63d81/538c916de4b0c2f4bdcd5360/1432928988640/IMG_0670.JPG')
+Task.create(title: 'hacer un asado', photo: 'http://www.infocampo.com.ar/wp-content/uploads/2017/05/asado.jpg')
+Task.create(title: 'Comer anticuchos', photo: 'http://www.lacocinachilena.tk/wp-content/uploads/2015/05/Anticuchos.jpg')
+Task.create(title: 'Bailar cueca', photo: 'http://img.soy-chile.cl/Fotos/2014/09/13/file_20140913171312.jpg')
+Task.create(title: 'Comer empanadas', photo: 'http://blog.hostalrioamazonas.cl/wp-content/uploads/2012/06/la-empanada-chilena.jpg')
