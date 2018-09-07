ce72526ef5d0af56d7c40fa00f5d0c8f0950c275
Gabriel Alarcon <galarconesp@gmail.com>
2018-09-06 21:59:53 -0300

    commit 07: user's status_task view completed

---
+++ app/controllers/user_tasks_controller.rb
@@ -0,0 +1,21 @@
+class UserTasksController < ApplicationController
+  def task_status
+    @user = User.find(params[:id])
+    @tasks = Task.all - @user.tasks
+    @user_tasks = @user.tasks
+  end
+
+  def complete_task
+    @user = User.find(params[:id])
+    @task = Task.find(params[:task_id])
+    @user.tasks << @task
+    redirect_to task_status_path(@user)
+  end
+
+  def remove_task
+    @user = User.find(params[:id])
+    @task = Task.find(params[:task_id])
+    @user.tasks.delete(@task)
+    redirect_to task_status_path(@user)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
   	registrations: 'users/registrations'
   }
+
+  get 'users/:id/task_status', to: 'user_tasks#task_status', as: 'task_status'
+  post 'users/:id/complete_task', to: 'user_tasks#complete_task', as: 'complete_task'
+  delete 'users/:id/remove_task', to: 'user_tasks#remove_task', as: 'remove_task'
+
   resources :tasks
   root 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
