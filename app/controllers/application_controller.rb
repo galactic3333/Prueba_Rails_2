d9bf173c953595dd6e718c3f5ba894db5f5491ad
Gabriel Alarcon <galarconesp@gmail.com>
2018-09-09 03:07:48 -0300

    commit 13: Translation to spanish ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   def show_count
     @count = params[:param]
   end
+
+  def after_sign_in_path_for(resource)
+    task_status_path(resource)
+  end
 end
