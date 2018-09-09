70214523ae53443584c19c5cd31f94ae882ed5b7
Gabriel Alarcon <galarconesp@gmail.com>
2018-09-09 03:35:50 -0300

    commit 14: description added to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_06_203015) do
+ActiveRecord::Schema.define(version: 2018_09_09_061511) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "user_tasks", force: :cascade do |t|
